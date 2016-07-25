7e87d39866ac6cf469b2c173b5aaa5a14de5773b
Mathew Engel <matsengel@gmail.com>
2016-07-25 13:07:52 -0700

    added a validation so that only 3 appointments can be booked on any given date

--- client/js/controllers/new_appointmentCtrl.js
+++ client/js/controllers/new_appointmentCtrl.js
@@ -11,18 +11,52 @@ function new_appointmentCtrl($scope, appointmentsFctry, userFctry, $routeParams)
     }
 
     getUser();
-        
-        
 
-    $scope.create = function(newAppointment, currentUser){
+    function getAppointments(){
+        console.log("getting appointments...")
+        appointmentsFctry.index(function(response){
+            console.log("response from appointmentsFctry.index", response)
+            $scope.appointments = response.data;
+        })
+    }
+    getAppointments();
+    
+    function noMoreThanThree(appointment, appointments){
+        var moreThanThree = false;
+        angular.forEach(appointments, function(oldAppointment){
+            if(appointment.date == oldAppointment.date){
+                console.log(appointment.date, "matches", oldAppointment.date)
+                moreThanThree = true;
+            }
+            else{
+                moreThanThree = false;
+            }    
+        })     
+        return(moreThanThree)
+    }  
+
+     $scope.tooMany = function(){
+        if($scope.msg){
+            return true;
+        }
+        return false;
+    }
+
+    $scope.create = function(newAppointment, currentUser, appointments){
            $scope.newAppointment = {};
             // newAppointment._user = currentUser
-            newAppointment.name = currentUser.name
-            console.log('scope.create method fires!!!', newAppointment)
-            appointmentsFctry.create(newAppointment, function (response){
-                console.log("response from apppointmentsFctrt.create, but from controller call", response)
-            })
-            $scope.newAppointment = {}; 
-        
+            noMoreThanThree(newAppointment, appointments)
+            if(!noMoreThanThree){
+                $scope.msg = null;
+                newAppointment.name = currentUser.name
+                console.log('scope.create method fires!!!', newAppointment)
+                appointmentsFctry.create(newAppointment, function (response){
+                    console.log("response from apppointmentsFctrt.create, but from controller call", response)
+                })
+                $scope.newAppointment = {}; 
+            }
+            else{
+                $scope.msg = {msg: "there are already to many appointments on that date"}
+            }
     }
 }
